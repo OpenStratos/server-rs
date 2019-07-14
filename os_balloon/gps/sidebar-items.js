bcf3e8ca2523eaf4639e31ea7913883b68f63cb4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-14 20:24:48 +0000

    Deploy OpenStratos/server-rs to github.com/OpenStratos/server-rs.git:gh-pages

--- os_balloon/gps/sidebar-items.js
+++ os_balloon/gps/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["FixStatus","GPS fix status."]],"static":[["GPS",""]],"struct":[["GPS","GPS data for concurrent check."],["Gps","GPS information structure."]]});
+initSidebarItems({"enum":[["FixStatus","GPS fix status."]],"static":[["GPS",""]],"struct":[["Frame","This structure represents a GPS frame."],["GPS","GPS data for concurrent check."],["Gps","GPS information structure."]]});
